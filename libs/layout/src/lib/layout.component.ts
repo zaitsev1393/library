53e2d9d8108781c2d0d05b59c9a2fb713fb3c9d6
Glib Zaycev <zaitsev1393@gmail.com>
2026-09-14 17:30:49 +0300

    feat(LIB-3): added flowbite and a header

--- libs/layout/src/lib/layout.component.ts
+++ libs/layout/src/lib/layout.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  template: `<h1>Dialog</h1>`,
+})
+export class DialogComponent {}
+
 @Component({
   selector: 'lib-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
+  imports: [HeaderComponent],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  private dialog = inject(Dialog);
+}
