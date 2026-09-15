87ac53d0ffa497de036f4414f9993021526dda84
Glib Zaycev <zaitsev1393@gmail.com>
2026-09-15 14:35:10 +0300

    feat(LIB-8): added menus for edit and delete

--- libs/shelf/src/lib/list/list.component.ts
+++ libs/shelf/src/lib/list/list.component.ts
@@ -1,3 +1,4 @@
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { Component, inject } from '@angular/core';
 import { ShelfStore } from '../shelf.store';
 
@@ -5,9 +6,20 @@ import { ShelfStore } from '../shelf.store';
   selector: 'lib-list-view',
   templateUrl: 'list.component.html',
   styleUrls: ['list.component.scss'],
+  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem],
 })
 export class ListComponent {
   private readonly shelfStore = inject(ShelfStore);
   public readonly books = this.shelfStore.books;
   public readonly filteredBooks = this.shelfStore.filteredBooks;
+
+  editBook(book: any) {
+    console.log(book);
+    // this.shelfStore.editBook(book);
+  }
+
+  deleteBook(book: any) {
+    console.log(book);
+    // this.shelfStore.deleteBook(book);
+  }
 }
