f8ff291acc67dd26870dce77896e3b1c66bbac05
Glib Zaycev <zaitsev1393@gmail.com>
2026-09-15 12:22:25 +0300

    feat(LIB-5): added add book and import book buttons

--- libs/shelf/src/lib/shelf/shelf.ts
+++ libs/shelf/src/lib/shelf/shelf.ts
@@ -1,14 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '@org/shared';
 import { ShelfStore } from '../shelf.store';
 import { ViewSwitcherComponent } from '../view-switcher/view-switcher.component';
 
 @Component({
   selector: 'lib-shelf',
   templateUrl: './shelf.html',
-  imports: [RouterOutlet, ViewSwitcherComponent],
+  imports: [RouterOutlet, ViewSwitcherComponent, ButtonComponent],
   providers: [ShelfStore],
 })
 export class ShelfComponent {
   public readonly shelfStore = inject(ShelfStore);
+
+  addBook(): void {
+    console.log('Add book');
+  }
+
+  importBooks(type: string): void {
+    console.log('Import books');
+  }
 }
